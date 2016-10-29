fc993765a4c3a1c0255ea9c44c0caf61c634afef
Manuel M <manu4216@gmail.com>
2016-10-29 22:02:56 +0000

    first commit

---
+++ hello_world.js
@@ -0,0 +1,12 @@
+/* 
+# LEARN YOU THE NODE.JS FOR MUCH WIN!  
+
+## HELLO WORLD (Exercise 1 of 13)  
+   
+  Write a program that prints the text "HELLO WORLD" to the console (stdout).  
+*/
+
+
+// Solution:
+
+console.log("HELLO WORLD");
